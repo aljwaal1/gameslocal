d6915feddd39386088d886b4b9f3d3e0e95e626f
aljwaal1 <yaya15112016@gmail.com>
2026-07-09 19:55:31 +0300

    Allow games to receive network core

--- lib/core/game_definition.dart
+++ lib/core/game_definition.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/widgets.dart';
 
+import 'network/local_network_core.dart';
+
+typedef GameScreenBuilder = Widget Function(BuildContext context, LocalNetworkCore? networkCore);
+
 class GameDefinition {
   const GameDefinition({
     required this.id,
@@ -13,5 +17,5 @@ class GameDefinition {
   final String name;
   final String playersText;
   final String status;
-  final WidgetBuilder builder;
+  final GameScreenBuilder builder;
 }
